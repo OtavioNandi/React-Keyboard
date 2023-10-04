b4b14e6e37013869b912832a48665e0fc73aba48
otavio <otavioluiznandi@gmail.com>
2023-10-04 08:58:26 -0300

    scss files updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,30 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import './App.scss';
 import TextArea from './components';
 // import DateTime from './DateTime';
 
 function App() {
-	const [comments, setComments] = useState([])
+	// const [comments, setComments] = useState([])
 
-	useEffect(() => {
-		fetch('http://localhost:5000/comments', {
-			method: 'GET',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		})
-		.then((resp) => resp.json())
-		.then((data) => {
-			setComments(data)
-		})
-		.catch((err) => console.log(err))
-	}, [])
+	// useEffect(() => {
+	// 	fetch('http://localhost:5000/comments', {
+	// 		method: 'GET',
+	// 		headers: {
+	// 			'Content-Type': 'application/json'
+	// 		},
+	// 	})
+	// 	.then((resp) => resp.json())
+	// 	.then((data) => {
+	// 		setComments(data)
+	// 	})
+	// 	.catch((err) => console.log(err))
+	// }, [])
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				{comments.length > 0 && comments.map((comment) => (
+				{/* {comments.length > 0 && comments.map((comment) => (
 					<p key={comment.id}>{comment.text}</p>
-				))}
+				))} */}
 				<TextArea />
 				{/* <DateTime /> */}
 			</header>
